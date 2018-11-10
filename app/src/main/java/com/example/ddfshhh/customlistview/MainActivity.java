73c2767a0e8f670f98f6931fcb9dfcc941cc9284
ByungHwa <byunghwara@gmail.com>
2018-11-10 22:03:32 +0800

    first commit

---
+++ app/src/main/java/com/example/ddfshhh/customlistview/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.ddfshhh.customlistview;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CustomListView listView1 = findViewById(R.id.list1);
+        final CustomListView listView2 = findViewById(R.id.list2);
+        final CustomListView listView3 = findViewById(R.id.list3);
+        final CustomListView listView4 = findViewById(R.id.list4);
+        final CustomListView listView5 = findViewById(R.id.list5);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listView1.setAdapter(adapter);
+        listView2.setAdapter(adapter);
+        listView3.setAdapter(adapter);
+        listView4.setAdapter(adapter);
+        listView5.setAdapter(adapter);
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+}
